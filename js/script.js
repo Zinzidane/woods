8c0f8828611b96a2b3c079e2562be4629269f8a5
Zinzidane <ya-gans@mail.ru>
2018-08-04 21:31:34 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Навигация на мобильных устройствах
+var navMain = document.querySelector(".main-nav");
+var navToggler = document.querySelector(".main-nav__toggler");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggler.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
